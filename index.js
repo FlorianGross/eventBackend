efd9ad772295de99538ebeb8a44ad7fae15e94b7
florian <floriangross@hotmail.de>
2022-06-02 14:22:06 +0200

    Cors

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-var corsOptions = {
-    origin: 'https://webprogevent.herokuapp.com',
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
