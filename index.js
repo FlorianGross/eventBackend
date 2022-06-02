026b4996e163da0915559b125c7f0289928f094d
florian <floriangross@hotmail.de>
2022-06-02 13:56:13 +0200

    Added cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 var corsOptions = {
-    origin: 'http://localhost:8081'
+    origin: 'https://webprogevent.herokuapp.com/api/auth/signin',
 };
 app.use(cors(corsOptions));
 app.use(express.json());
