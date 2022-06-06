cc982ec8d67841b547150048cf63d39dc6e4be01
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2022-06-06 15:43:32 +0200

    Added getAllUsers function

---
+++ controllers/user.controller.js
@@ -0,0 +1,13 @@
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+exports.getAllUsers = (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(users);
+    });
+}
+
