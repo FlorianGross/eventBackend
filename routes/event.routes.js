8f2763a93f93fbd7120f2b90a5acbc9f21fcab84
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2022-06-10 13:45:07 +0200

    Fix

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
         );
         next();
     });
-    
+    app.post("/api/events", [authJwt.verifyToken], controller.getAllEvents);
     app.post("/api/event/create", [authJwt.verifyToken], controller.create);
     app.post("/api/event/change", [authJwt.verifyToken], controller.change);
     app.post("/api/event/delete", [authJwt.verifyToken], controller.delete);
