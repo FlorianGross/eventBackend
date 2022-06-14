c9359365f28b0f8f4ca0781eef7c5de08fa0edab
florian <floriangross@hotmail.de>
2022-06-14 16:52:16 +0200

    Fix

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,13 +1,12 @@
 // https://www.bezkoder.com/node-js-express-file-upload/
-
 const util = require('util');
 const multer = require('multer');
 const maxSize = 2 * 1024 * 1024;
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, __basedir + '/uploads/');
-    }
-    , filename: function (req, file, cb) {
+    }, 
+    filename: function (req, file, cb) {
         cb(null, file.originalname);
     }
 }
