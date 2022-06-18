f1de1a467a7875f41bb47c9a27b20ffaa4c56147
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2022-06-18 11:59:00 +0200

    Fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,35 +9,14 @@ exports.signup = (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 8),
-        
+
     });
     user.save((err, user) => {
         if (err) {
             res.status(500).send({ message: err });
             return;
-        }
-        if (req.body.roles) {
-            Role.find(
-                {
-                    name: { $in: req.body.roles }
-                },
-                (err, roles) => {
-                    if (err) {
-                        res.status(500).send({ message: err });
-                        return;
-                    }
-                    user.roles = roles.map(role => role._id);
-                    user.save(err => {
-                        if (err) {
-                            res.status(500).send({ message: err });
-                            return;
-                        }
-                        res.send({ message: "User was registered successfully!" });
-                    });
-                }
-            );
         } else {
-            Role.findOne({ name: "user" }, (err, role) => {
+            Role.findOne({ name: "User" }, (err, role) => {
                 if (err) {
                     res.status(500).send({ message: err });
                     return;
